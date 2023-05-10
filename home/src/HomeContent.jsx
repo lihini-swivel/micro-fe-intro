46b02a33a673e0fde00db5e2aa1027a0983b63f5
lihini-swivel <lihini@swivelgroup.com.au>
2023-05-10 08:41:14 +0530

    Implement sharing cart in items

--- home/src/HomeContent.jsx
+++ home/src/HomeContent.jsx
@@ -2,10 +2,10 @@ import React, { useState, useEffect } from 'react'
 // import { Link } from 'react-router-dom'
 
 import { getProducts, currency } from './products'
-// import { addToCart, useLoggedIn } from 'cart/cart'
+import { addToCart, useLoggedIn } from 'cart/cart'
 
 export default function HomeContent() {
-  // const loggedIn = useLoggedIn()
+  const loggedIn = useLoggedIn()
   const [products, setProducts] = useState([])
 
   useEffect(() => {
@@ -29,7 +29,7 @@ export default function HomeContent() {
             <div className='flex-end'>{currency.format(product.price)}</div>
           </div>
           <div className='text-sm mt-4'>{product.description}</div>
-          {/* {loggedIn && (
+          {!loggedIn && (
             <div className='text-right mt-2'>
               <button
                 className='bg-blue-500 hover:bg-blue-700 text-white text-sm font-bold py-2 px-4 rounded'
@@ -39,7 +39,7 @@ export default function HomeContent() {
                 Add to Cart
               </button>
             </div>
-          )} */}
+          )}
         </div>
       ))}
     </div>
